39bc602ed3e9644b4b77c67d6a8257238c487ced
thiago <thiagofreitas1106@gmail.com>
2024-08-14 16:10:44 -0300

    new project

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,45 +1,44 @@
-import React from "react";
 import { Grid, Typography } from "@mui/material";
 import CustomCard from "./card";
 
 const projectsData = [
   {
-    title: "Barba Brutal",
-    images: generateImageArray("barba", 4),
-    access: "https://barba-brutal-frontend-phi.vercel.app",
+    title: "WR Café Bar",
+    images: generateImageArray("wr", 10),
+    access: "https://wr-cafe-bar.vercel.app/",
     description:
-      `Sistema que ainda estou construindo de barbearia com imagens geradas por inteligência artificial, possibilidade de agendar o corte escolhendo o barbeiro com hora marcada.`,
+    `Site que reformulei para um barzinho da cidade de Maringá tanto a versão mobile como desktop, nas fotos você poderá como foi o resultado final, e como era o site antes!`,
     technologies:
-      "Tecnologias: Turbo, Next, React, React Native, Javascript, Typescript, Tailwind, Prisma, Nest, Node, PostgreSQL",
+    "Tecnologias: React, Javascript, Material-UI, HTML, CSS",
   },
   {
     title: "Prometheus IA",
     images: generateImageArray("pro", 7),
     access: "https://prometheus-jet-one.vercel.app",
     description:
-      `Página com design moderno para oferecer serviços de inteligência artificial`,
+    `Página com design moderno para oferecer serviços de inteligência artificial`,
     technologies:
-      "Tecnologias: React, Javascript, Tailwind, HTML, CSS",
+    "Tecnologias: React, Javascript, Tailwind, HTML, CSS",
   },
   {
     title: "Client Manager",
     images: generateImageArray("cmanager", 3),
     access: "https://front-client-manager.vercel.app",
     description:
-      `Este é um sistema para gerenciar cadastro de clientes, nele desenvolvi uma tabela onde são listados todos os clientes cadastrados, também temos um
-      formulário de cadastro com validação, funções para deletar ou editar clientes, notificação para visualização de atividades recentes como: novos clientes
-      cadastrados, deletados ou atualizados, também podemos buscar os clientes pelo nome e visualizar gráficos na página inicial de acordo com os dados atuais disponíveis no nosso banco de dados!`,
+    `Este é um sistema para gerenciar cadastro de clientes, nele desenvolvi uma tabela onde são listados todos os clientes cadastrados, também temos um
+    formulário de cadastro com validação, funções para deletar ou editar clientes, notificação para visualização de atividades recentes como: novos clientes
+    cadastrados, deletados ou atualizados, também podemos buscar os clientes pelo nome e visualizar gráficos na página inicial de acordo com os dados atuais disponíveis no nosso banco de dados!`,
     technologies:
-      "Tecnologias: React, Javascript, React-hook-form, HTML, CSS, Redux, Chart.js, Material-UI, Node, MongoDB",
+    "Tecnologias: React, Javascript, React-hook-form, HTML, CSS, Redux, Chart.js, Material-UI, Node, MongoDB",
   },
   {
     title: "Lenx Delivery",
     images: generateImageArray("delivery", 3),
     access: "https://front-lenxdelivery.vercel.app",
     description:
-      "Site de restaurante, com cardápio, pedidos e localização ao vivo.",
+    "Site de restaurante, com cardápio, pedidos e localização ao vivo.",
     technologies:
-      "Tecnologias: React, Javascript, HTML, CSS, Material-UI, Node, MongoDB, MapBox",
+    "Tecnologias: React, Javascript, HTML, CSS, Material-UI, Node, MongoDB, MapBox",
   },
   {
     title: "Curious Places",
@@ -47,21 +46,31 @@ const projectsData = [
     access: "https://curious-places.vercel.app",
     description: "Site sobre alguns dos locais mais curiosos do mundo.",
     technologies: "Tecnologias: React, Javascript, HTML, CSS",
-  },
-  {
-    title: "QRCode generator",
-    images: generateImageArray("qrcode", 1),
-    access: "https://qr-code-generator-phi-six.vercel.app",
-    description:
-      "Este é um gerador de QRCode simples e prático, adicionei também um histórico que salva automaticamente conforme você vai gerando seus códigos.",
-    technologies:
-      "Tecnologias: React, Javascript, HTML, CSS",
-  },
+  }
+  
   // {
-  //   title: "Pokemon Knowledge",
-  //   images: generateImageArray("poke", 5),
-  //   access: "https://poke-wiki-front.vercel.app",
+  //   title: "Barba Brutal",
+  //   images: generateImageArray("barba", 4),
+  //   access: "https://barba-brutal-frontend-phi.vercel.app",
   //   description:
+  //     `Sistema que ainda estou construindo de barbearia com imagens geradas por inteligência artificial, possibilidade de agendar o corte escolhendo o barbeiro com hora marcada.`,
+  //   technologies:
+  //     "Tecnologias: Turbo, Next, React, React Native, Javascript, Typescript, Tailwind, Prisma, Nest, Node, PostgreSQL",
+  // },
+  // {
+    //   title: "QRCode generator",
+    //   images: generateImageArray("qrcode", 1),
+    //   access: "https://qr-code-generator-phi-six.vercel.app",
+    //   description:
+    //     "Este é um gerador de QRCode simples e prático, adicionei também um histórico que salva automaticamente conforme você vai gerando seus códigos.",
+    //   technologies:
+    //     "Tecnologias: React, Javascript, HTML, CSS",
+    // },
+    // {
+      //   title: "Pokemon Knowledge",
+      //   images: generateImageArray("poke", 5),
+      //   access: "https://poke-wiki-front.vercel.app",
+      //   description:
   //     "Neste site podemos visualizar todos os detalhes dos pokémon existentes consumindo a PokeAPI.",
   //   technologies:
   //     "Tecnologias: Next, React, Javascript, HTML, CSS, Material-UI",
@@ -75,13 +84,13 @@ const projectsData = [
   //   technologies:
   //     "Tecnologias: React, Vite, Javascript, HTML, CSS, Material-UI",
   // },
-  {
-    title: "Pokedex",
-    images: generateImageArray("pokedex", 1),
-    access: "https://pokedex-mu-ten.vercel.app",
-    description: "Pokedex com sprites animadas",
-    technologies: "Tecnologias: Javascript, HTML, CSS",
-  },
+  // {
+  //   title: "Pokedex",
+  //   images: generateImageArray("pokedex", 1),
+  //   access: "https://pokedex-mu-ten.vercel.app",
+  //   description: "Pokedex com sprites animadas",
+  //   technologies: "Tecnologias: Javascript, HTML, CSS",
+  // },
 ];
 
 function generateImageArray(prefix, count) {
